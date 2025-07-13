d8e5cf528d8576bc245f0418e88cfcc6fbc0619a
Fabian Petersen <fabian@nf-petersen.de>
2025-07-13 22:06:55 +0200

    feat: add sentinelCtx for refactor of frontend

---
+++ web/types.go
@@ -0,0 +1,143 @@
+//go:generate go tool go-enum --marshal --values -f ./*.go
+package web
+
+import (
+	"encoding/json"
+	"html/template"
+	"strings"
+)
+
+// ENUM(state, code, id_token, access_token, expires_in, refresh_token, refresh_expires_in, token_type)
+type TokenResponseField string
+
+// ENUM(info, success, error, warning)
+type MessageType string
+
+// ENUM(login.tmpl, error.tmpl, info.tmpl, form-redirect.tmpl, form-post.tmpl)
+type PageID string
+
+func ToJsDeclaration(v any, indent int) (template.JS, error) {
+	var buf strings.Builder
+	enc := json.NewEncoder(&buf)
+	enc.SetIndent(strings.Repeat(" ", indent), "  ")
+	if err := enc.Encode(v); err != nil {
+		return "", err
+	}
+	return template.JS(buf.String()), nil
+}
+
+type Message struct {
+	Type    MessageType `json:"type"`
+	Summary string      `json:"summary"`
+}
+
+type URLs struct {
+	BasePath     string `json:"basePath"`
+	ResourcePath string `json:"resourcePath"`
+}
+
+// type Client struct {
+// 	ClientID string `json:"clientId"`
+// 	Name     string `json:"name"`
+// }
+
+type SentinelCtx struct {
+	PageID   PageID    `json:"pageId"`
+	Message  *Message  `json:"message"`
+	Messages []Message `json:"messages"`
+	URLs     URLs      `json:"urls"`
+	// Client   Client    `json:"client"`
+}
+
+func NewSentinelCtx(basePath string, message *Message, messages []Message) SentinelCtx {
+	return SentinelCtx{
+		Message:  message,
+		Messages: messages,
+		URLs: URLs{
+			BasePath:     basePath,
+			ResourcePath: basePath + "/assets",
+		},
+	}
+}
+
+type Provider struct {
+	LoginURL    string `json:"loginUrl"`
+	Alias       string `json:"alias"`
+	ProviderID  string `json:"providerId"`
+	DisplayName string `json:"displayName"`
+	IconPath    string `json:"icon"`
+}
+
+type CSRF struct {
+	FieldName string `json:"fieldName"`
+	Value     string `json:"value"`
+}
+
+type LoginSentinelCtx struct {
+	SentinelCtx
+	Providers []Provider `json:"providers"`
+	CSRF      CSRF       `json:"csrf"`
+}
+
+func NewLoginSentinelCtx(sentinelCtx SentinelCtx, providers []Provider, csrf CSRF) LoginSentinelCtx {
+	sentinelCtx.PageID = "login.tmpl"
+	return LoginSentinelCtx{
+		SentinelCtx: sentinelCtx,
+		Providers:   providers,
+		CSRF:        csrf,
+	}
+}
+
+type FormRedirectSentinelCtx struct {
+	SentinelCtx
+	RedirectURL string `json:"redirectUrl"`
+}
+
+func NewFormRedirectSentinelCtx(sentinelCtx SentinelCtx, redirectURI string) FormRedirectSentinelCtx {
+	sentinelCtx.PageID = "form-redirect.tmpl"
+	return FormRedirectSentinelCtx{
+		SentinelCtx: sentinelCtx,
+		RedirectURL: redirectURI,
+	}
+}
+
+type FormPostSentinelCtx struct {
+	SentinelCtx
+	RedirectURL string `json:"redirectUrl"`
+}
+
+func NewFormPostSentinelCtx(sentinelCtx SentinelCtx, redirectURI string) FormPostSentinelCtx {
+	sentinelCtx.PageID = "form-post.tmpl"
+	return FormPostSentinelCtx{
+		SentinelCtx: sentinelCtx,
+		RedirectURL: redirectURI,
+	}
+}
+
+type InfoSentinelCtx struct {
+	SentinelCtx
+	Message Message `json:"message"`
+}
+
+func NewInfoSentinelCtx(sentinelCtx SentinelCtx, message Message) InfoSentinelCtx {
+	sentinelCtx.PageID = "info.tmpl"
+	sentinelCtx.Message = &message
+	return InfoSentinelCtx{
+		SentinelCtx: sentinelCtx,
+		Message:     message,
+	}
+}
+
+type ErrorSentinelCtx struct {
+	SentinelCtx
+	Message Message `json:"message"`
+}
+
+func NewErrorSentinelCtx(sentinelCtx SentinelCtx, message Message) ErrorSentinelCtx {
+	sentinelCtx.PageID = "error.tmpl"
+	sentinelCtx.Message = &message
+	return ErrorSentinelCtx{
+		SentinelCtx: sentinelCtx,
+		Message:     message,
+	}
+}
